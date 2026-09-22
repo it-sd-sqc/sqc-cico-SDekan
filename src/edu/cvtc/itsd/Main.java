10d1d81823d4b920d0993a7254d8920eb1656d1d
Steven Dekan <sdekan5@student.cvtc.edu>
2026-09-22 18:52:54 -0500

    Added changes for Ticket 502

--- src/edu/cvtc/itsd/Main.java
+++ src/edu/cvtc/itsd/Main.java
@@ -41,7 +41,7 @@ public class Main {
     public void insertString(FilterBypass fb, int offset, String stringToAdd, AttributeSet attr)
         throws BadLocationException
     {
-      if (fb.getDocument() != null && stringToAdd.matches("\\d+")) {
+      if (fb.getDocument() != null && (stringToAdd.matches("\\d+") || stringToAdd.isEmpty())) {
         super.insertString(fb, offset, stringToAdd, attr);
       }
       else {
@@ -53,7 +53,7 @@ public class Main {
     public void replace(FilterBypass fb, int offset, int lengthToDelete, String stringToAdd, AttributeSet attr)
         throws BadLocationException
     {
-      if (fb.getDocument() != null && stringToAdd.matches("\\d+")) {
+      if (fb.getDocument() != null && (stringToAdd.matches("\\d+")  || stringToAdd.isEmpty())) {
         super.replace(fb, offset, lengthToDelete, stringToAdd, attr);
       }
       else {
@@ -230,6 +230,7 @@ public class Main {
     frame.setMinimumSize(new Dimension(320, 240));
     frame.setPreferredSize(new Dimension(640, 480));
     frame.setMaximumSize(new Dimension(640, 480));
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     // Collect each "card" panel in a deck.
     deck = new JPanel(new CardLayout());
